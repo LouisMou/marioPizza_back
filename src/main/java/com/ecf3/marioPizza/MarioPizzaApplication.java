3730180f52e5f8dfa04ed2367df3b808526c2d9e
Louis MOUILLE <louismou04@gmail.com>
2024-04-04 10:41:25 +0200

    [+] added back project

---
+++ src/main/java/com/ecf3/marioPizza/MarioPizzaApplication.java
@@ -0,0 +1,13 @@
+package com.ecf3.marioPizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarioPizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarioPizzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecf3/marioPizza/MarioPizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecf3.marioPizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarioPizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
